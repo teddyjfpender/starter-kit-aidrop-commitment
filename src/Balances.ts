5d49554a869a061e7d279050d06a151842b8e10b
teddyjfpender <teddyjfpender@gmail.com>
2023-10-14 16:41:29 +0100

    init repo

---
+++ src/Balances.ts
@@ -0,0 +1,77 @@
+import {
+  RuntimeModule,
+  runtimeModule,
+  state,
+  runtimeMethod,
+} from "@proto-kit/module";
+
+import { State, StateMap, assert } from "@proto-kit/protocol";
+import { Provable, PublicKey, UInt64 } from "snarkyjs";
+
+interface BalancesConfig {
+  totalSupply: UInt64;
+}
+
+@runtimeModule()
+export class Balances extends RuntimeModule<BalancesConfig> {
+  @state() public balances = StateMap.from<PublicKey, UInt64>(
+    PublicKey,
+    UInt64
+  );
+
+  @state() public circulatingSupply = State.from<UInt64>(UInt64);
+
+  @runtimeMethod()
+  public setBalance(address: PublicKey, amount: UInt64) {
+    const circulatingSupply = this.circulatingSupply.get();
+    const newCirculatingSupply = circulatingSupply.value.add(amount);
+
+    assert(
+      newCirculatingSupply.lessThanOrEqual(this.config.totalSupply),
+      "Circulating supply would be higher than total supply"
+    );
+
+    this.circulatingSupply.set(newCirculatingSupply);
+
+    const currentBalance = this.balances.get(address);
+    const newBalance = currentBalance.value.add(amount);
+
+    this.balances.set(address, newBalance);
+  }
+
+  /**
+     * Transfer tokens from the transaction sender to a given address.
+     * 
+     * @param address - The public key of the recipient address.
+     * @param amount - The amount to transfer.
+     * 
+     * @remarks
+     * This method ensures that the sender has sufficient balance before transferring.
+     * The balances of the sender and recipient are updated accordingly.
+     */
+  @runtimeMethod()
+  public sendTo(address: PublicKey, amount: UInt64) {
+      // Retrieve the public key of the transaction sender
+      const sender = this.transaction.sender;
+      
+      // Fetch current balance of the sender and recipient from the state
+      const senderBalance = this.balances.get(sender);
+      const recipientBalance = this.balances.get(address);
+
+      // Ensure the sender has a balance that is greater than or equal to the amount they wish to send
+      assert(
+        senderBalance.value.greaterThanOrEqual(amount),
+        "Sender does not have enough balance"
+      );
+
+      const senderBalanceTrue = Provable.if(
+          senderBalance.value.greaterThanOrEqual(amount),
+          senderBalance.value,
+          senderBalance.value.add(amount)
+      );
+      
+      // Deduct the specified amount from the sender's balance and add it to the recipient's balance
+      this.balances.set(sender, senderBalanceTrue.sub(amount));
+      this.balances.set(address, recipientBalance.value.add(amount));
+  }
+}
---
+++ test/Airdrop.test.ts
@@ -0,0 +1,145 @@
+import { TestingAppChain } from "@proto-kit/sdk";
+import { MerkleMap, Poseidon, PrivateKey, PublicKey, UInt64 } from "snarkyjs";
+import { Balances } from "../src/Balances";
+import { Airdrop } from "../src/Airdrop";
+
+
+describe("Balances", () => {
+  let appChain: TestingAppChain<{ Balances: typeof Balances; Airdrop: typeof Airdrop}>;
+  let totalSupply: UInt64;
+  let alicePrivateKey: PrivateKey;
+  let alicePublicKey: PublicKey;
+  let bobPrivateKey: PrivateKey;
+  let bobPublicKey: PublicKey;
+  let airdropTree: MerkleMap;
+  let airdropAmount: UInt64;
+  
+  beforeAll(() => {
+    totalSupply = UInt64.from(10_000);
+
+    appChain = TestingAppChain.fromRuntime({
+      modules: {
+        Balances,
+        Airdrop,
+      },
+      config: {
+        Balances: {
+          totalSupply,
+        },
+        Airdrop: {} as unknown
+      },
+    });
+
+    const PRIVATE_KEY_0 = "EKE1h2CEeYQxDaPfjSGNNB83tXYSpn3Cqt6B3vLBHuLixZLApCpd"
+    const PRIVATE_KEY_1 = "EKEU31uonuF2rhG5f8KW4hRseqDjpPVysqcfKCKxqvs7x5oRviN1"
+
+    alicePrivateKey = PrivateKey.fromBase58(PRIVATE_KEY_0);
+    alicePublicKey = alicePrivateKey.toPublicKey();
+    bobPrivateKey = PrivateKey.fromBase58(PRIVATE_KEY_1);
+    bobPublicKey = bobPrivateKey.toPublicKey();
+
+    // the desired airdrop amount for bob
+    airdropAmount = UInt64.from(1000);
+
+    // populate the airdrop tree
+    airdropTree = new MerkleMap();
+    airdropTree.set(
+      // the key is the hash of the public key
+      Poseidon.hash(bobPublicKey.toFields()),
+      Poseidon.hash(UInt64.from(airdropAmount).toFields())
+    );
+  })
+
+  it("should demonstrate how balances work", async () => {
+
+    await appChain.start();
+
+    appChain.setSigner(alicePrivateKey);
+    // this is almost equivalent to obtaining the contract ABI in solidity
+    const balances = appChain.runtime.resolve("Balances");
+
+    const tx1 = appChain.transaction(alicePublicKey, () => {
+      balances.setBalance(alicePublicKey, UInt64.from(1000));
+    });
+
+    await tx1.sign();
+    await tx1.send();
+
+    const startTime = new Date().getTime();
+    const block1 = await appChain.produceBlock();
+    const endTime = new Date().getTime();
+    console.log(`Block Production time: ${endTime - startTime} milliseconds`);
+
+    const aliceBalance = await appChain.query.runtime.Balances.balances.get(
+      alicePublicKey
+    );
+
+    expect(block1?.txs[0].status).toBe(true);
+    expect(aliceBalance?.toBigInt()).toBe(1000n);
+
+    // send tokens to Bob
+    const bobPrivateKey = PrivateKey.random();
+
+    const tx2 = appChain.transaction(alicePublicKey, () => {
+      balances.sendTo(bobPrivateKey.toPublicKey(), UInt64.from(100));
+    }, { nonce: 1 });
+
+    await tx2.sign();
+    await tx2.send();
+
+    const startTimeTx = new Date().getTime();
+    const block2 = await appChain.produceBlock();
+    const endTimeTx = new Date().getTime();
+    console.log(`Block Production time (1 txs): ${endTimeTx - startTimeTx} milliseconds`);
+
+    // check bob has 100 tokens
+    const bobBalance = await appChain.query.runtime.Balances.balances.get(
+      bobPrivateKey.toPublicKey()
+    )
+
+    expect(bobBalance?.toBigInt()).toBe(100n);
+
+
+    // resolve the airdrop module
+    const airdrop = appChain.runtime.resolve("Airdrop");
+
+    // set the rewards merkle tree root
+    const tx3 = appChain.transaction(alicePublicKey, () => {
+      airdrop.setAirdropCommitment(airdropTree.getRoot());
+    });
+
+    await tx3.sign();
+    await tx3.send();
+
+    const startTimeAirdrop = new Date().getTime();
+    const block3 = await appChain.produceBlock();
+    const endTimeAirdrop = new Date().getTime();
+    console.log(`Set Commitment, Block Production time (1 txs): ${endTimeAirdrop - startTimeAirdrop} milliseconds`);
+
+    const airdropCommitment = await appChain.query.runtime.Airdrop.commitment.get();
+    console.log("Airdrop Commitment: ", airdropCommitment?.toBigInt());
+    expect(airdropCommitment?.toBigInt()).toBe(airdropTree.getRoot().toBigInt());
+    expect(block3?.txs[0].status).toBe(true);
+
+    // bob claims his airdrop
+    const tx4 = appChain.transaction(bobPublicKey, () => {
+      airdrop.claim(airdropTree.getWitness(Poseidon.hash(bobPublicKey.toFields())), airdropAmount);
+    });
+
+    await tx4.sign();
+    await tx4.send();
+
+    const startTimeClaim = new Date().getTime();
+    const block4 = await appChain.produceBlock();
+    const endTimeClaim = new Date().getTime();
+    console.log(`Bob Claim, Block Production time (1 txs): ${endTimeClaim - startTimeClaim} milliseconds`);
+
+    expect(block4?.txs[0].status).toBe(true);
+    // get bob's balance
+    const bobBalanceAfterClaim = await appChain.query.runtime.Balances.balances.get(
+      bobPublicKey
+    );
+
+    expect(bobBalanceAfterClaim?.toBigInt()).toBe(1100n);
+  });
+});
